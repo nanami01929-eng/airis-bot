f6f233de690f424be35d83566b670f6e1978179e
nanami01929-eng <nanami01929@gmail.com>
2026-09-17 23:48:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ generation_config = {
 }
 
 model = genai.GenerativeModel(
-    model_name="gemini-2.5-flash",
+    model_name="gemini-3.6-flash",
     generation_config=generation_config,
     system_instruction=(
         "Ты — бот Айрис, созданная по образу Мисы Амане из аниме 'Тетрадь Смерти'. "
