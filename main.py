a24345335a353d742e418d43be219719c18da2b5
nanami01929-eng <nanami01929@gmail.com>
2026-09-18 00:05:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ import google.generativeai as genai
 logging.basicConfig(level=logging.INFO)
 
 BOT_TOKEN = "8984930047:AAFrfTQKMchyfNbhWCoO3Zbe4RYOkRSCQek"
-GEMINI_API_KEY = "AIzaSyCtubjX-63XGhzTJOkL_e3qUtSFL_EeBaE"
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 OWNER_ID = 8470088909  # Твой ID (Лайт-кун)
 
 bot = Bot(token=BOT_TOKEN)
